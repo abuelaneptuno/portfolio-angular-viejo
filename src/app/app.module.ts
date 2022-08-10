8a5a893d1f0cb12b8c66f4509ea22418fa1d08e9
Carmela <carmelalucuaranta@gmail.com>
2022-08-10 17:14:44 -0300

    hola toy rompido

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { EncabezadoComponent } from './componentes/encabezado/encabezado.component';
+import { FormacionComponent } from './componentes/formacion/formacion.component';
+import { CompetenciasComponent } from './componentes/competencias/competencias.component';
+import { ProyectosComponent } from './componentes/proyectos/proyectos.component';
+import { AcercaDeComponent } from './componentes/acerca-de/acerca-de.component';
+import { FooterComponent } from './componentes/footer/footer.component';
+import { HttpClientModule } from '@angular/common/http';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    EncabezadoComponent,
+    FormacionComponent,
+    CompetenciasComponent,
+    ProyectosComponent,
+    AcercaDeComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
